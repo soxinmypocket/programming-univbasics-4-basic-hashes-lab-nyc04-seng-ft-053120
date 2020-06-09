4b730a90fcec217ecbc94b58f47dc0d42cf419ac
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-09 19:04:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,16 +5,16 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  { favorite_color: "blue"}
+  { favorite_color: "blue" }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  { name: "Grace Hopper"}
+  { name: "Grace Hopper" }
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
-  { id: number}
+  { id: number }
 end
